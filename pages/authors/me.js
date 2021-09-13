d5817f82cf4bccbea927485f3219b936b6f8bed1
Carolina Rodriguez Torres <carol@askphill.com>
2021-09-13 10:22:17 +0200

    initial commit

---
+++ pages/authors/me.js
@@ -0,0 +1,4 @@
+export default function MeAuthor () {
+
+    return <h2>Me</h2>
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Date from '../../components/date'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+
+// You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
+export const getStaticPaths = () => {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async ({ params }) => {
+    const postData = await getPostData(params.id)  // your fetch function here
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+    </Layout>
+  )
+}
